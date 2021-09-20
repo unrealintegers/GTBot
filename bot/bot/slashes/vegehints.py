d041f6a7815c8b666066badb7a13da779ef7188e
UInt <unrealintegers@gmail.com>
2021-09-20 21:44:44 +1000

    restore vegehint functionality (Part 2)

--- bot/bot/slashes/vegehints.py
+++ bot/bot/slashes/vegehints.py
@@ -1,12 +1,11 @@
-import discord
+import asyncio
 import codecs
 import random
-from discord.ext import commands
-from discord_slash import ApplicationContext
-from discord_slash import cog_ext
-from discord_slash.utils.manage_commands import create_option
 
-from .utils import convert_args
+from discord import Guild, Embed
+from discord import ApplicationContext, Option
+from ..bot import DiscordBot, SlashCommand
+from ..utils import convert_args
 
 
 class Vegehint:
@@ -16,7 +15,7 @@ class Vegehint:
         self.id = num
 
     def to_embed(self):
-        embed = discord.Embed(title=self.name,
+        embed = Embed(title=self.name,
                               description=self.desc,
                               color=0x7bed24)
         embed.set_footer(text=f"Vegehint #{self.id}")
@@ -28,60 +27,65 @@ class Vegehint:
         return Vegehint(embed.title, embed.description, embed_id)
 
 
-class Vegehints(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+class Vegehints(SlashCommand):
+    def __init__(self, bot: DiscordBot, guild_ids: list[int]):
+        super().__init__(bot, guild_ids)
 
         self.vegehints = []
 
-    async def init_vegehints(self):
-        ch = self.bot.Vege.get_channel(836076080564535337)
+        self.bot.bot.slash_command(
+            guild_ids=[762888327161708615])(self.vegehint)
+
+        if vege := self.bot.bot.get_guild(762888327161708615):
+            asyncio.create_task(self.init_vegehints(vege))
+
+    async def init_vegehints(self, vege: Guild):
+        ch = vege.get_channel(836076080564535337)
         self.vegehints = list(map(lambda x: Vegehint.from_embed(x.embeds[0]),
                                   await ch.history().flatten()))
 
     def get_vegehint(self):
         return random.choice(self.vegehints)
 
-    @cog_ext.cog_slash(guild_ids=[762888327161708615],
-                       name="vegehint",
-                       description="Gets a random vegehint!",
-                       options=[
-                           create_option("flags", "Optional flags", str, False)
-                       ])
-    async def vegehint(self, ctx: ApplicationContext, flags: str = ""):
+    async def vegehint(
+            self, ctx: ApplicationContext,
+            flags: Option(str, "flags", required=False) = ""
+    ):
+        """Gets a random vegehint"""
         flags = convert_args(flags.split())
 
         if 'c' not in flags:
             try:
                 hint = self.get_vegehint()
             except IndexError:
-                await ctx.send("No vegehints available at this time.",
-                               hidden=True)
+                await ctx.respond("No vegehints available at this time.",
+                                  ephemeral=True)
             else:
-                await ctx.send(embed=hint.to_embed())
+                await ctx.respond(embed=hint.to_embed())
 
             return
 
-        elif 812998213543133204 in [r.id for r in ctx.author.roles]:
+        elif 812998213543133204 in [r.id for r in ctx.user.roles]:
             if 'n' in flags:
                 name = flags['n'][0]
             else:
-                await ctx.send("Name not specified. Please use `-n <name>`.",
-                               hidden=True)
+                await ctx.respond(
+                    "Name not specified. Please use `-n <name>`.",
+                    ephemeral=True)
                 return
 
             if 'd' in flags:
                 desc = flags['d'][0]
                 desc = codecs.decode(desc, "unicode-escape")
             else:
-                await ctx.send("Description not specified. Please use "
-                               "`-d <description>`.", hidden=True)
+                await ctx.respond("Description not specified. Please use "
+                                  "`-d <description>`.", ephemeral=True)
                 return
 
-            embed = discord.Embed(title=name, description=desc, color=0x7bed24)
+            embed = Embed(title=name, description=desc, color=0x7bed24)
             embed.set_footer(text=f"Vegehint #{len(self.vegehints) + 1}")
 
             self.vegehints.append(Vegehint.from_embed(embed))
 
             await ctx.channel.send(embed=embed)
-            await ctx.send("Done!", hidden=True)
+            await ctx.respond("Done!", ephemeral=True)
