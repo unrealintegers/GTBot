40b23db4893e73379621b8d2be4c6acd8fa16df4
unrealintegers <47798345+unrealintegers@users.noreply.github.com>
2022-01-13 14:11:12 +1100

    update apktool to auto-download

--- util/apktool.py
+++ util/apktool.py
@@ -1,11 +1,35 @@
 import json
 import os
+from bs4 import BeautifulSoup
+import cloudscraper
+import shutil
+import logging
+import zipfile
 
 import UnityPy
 
-files = [f for f in os.listdir() if f.endswith('.apk')]
+APKPURE_LINK = "https://apkpure.com/%EA%B0%80%EB%94%94%EC%96%B8-" \
+               "%ED%85%8C%EC%9D%BC%EC%A6%88/com.kakaogames.gdtskr/download"
 
-env = UnityPy.load(files[0])
+logging.root.setLevel(logging.INFO)
+
+logging.info("Finding APK")
+scraper = cloudscraper.create_scraper(interpreter='nodejs')
+res = scraper.get(APKPURE_LINK).text
+soup = BeautifulSoup(res, "html.parser").find('a', {'id': 'download_link'})
+if not soup['href']:
+    exit()
+
+logging.info("Downloading APK")
+with scraper.get(soup['href'], stream=True) as r:
+    with open('gdts.xapk', 'wb') as f:
+        shutil.copyfileobj(r.raw, f)
+
+logging.info("Loading APK")
+
+with zipfile.ZipFile("gdts.xapk") as z:
+    apk_name = z.extract("com.kakaogames.gdtskr.apk")
+env = UnityPy.load(apk_name)
 
 # grab the 2 relevant portrait files
 coord_raw = env.container['assets/spritesheets/story-portraits/portraits.json']
@@ -14,6 +38,7 @@ image_raw = env.container['assets/spritesheets/story-portraits/portraits.png']
 # read these files for dict & image
 coords = json.loads(coord_raw.read().m_Script.tobytes().decode('ascii'))
 image = image_raw.read().image
+image.show()
 
 # make a new directory if needed
 if not os.path.exists("portraits"):
